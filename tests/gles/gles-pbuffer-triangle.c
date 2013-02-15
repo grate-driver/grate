66a2807c06c106367f92c2db851127644e95d246
Thierry Reding <thierry@gilfi.de>
2013-02-15 07:43:42 +0100

    Add gles-pbuffer-triangle test program
    This test program draws a smoothly shaded 3-color triangle to a pbuffer
    and dumps the pbuffer contents to a PNG file.
    
    Signed-off-by: Thierry Reding <thierry@gilfi.de>

---
+++ tests/gles/gles-pbuffer-triangle.c
@@ -0,0 +1,111 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <GLES2/gl2.h>
+
+#include "common.h"
+
+static const GLchar *vertex_shader[] = {
+	"attribute vec4 position;\n",
+	"attribute vec4 color;\n",
+	"varying vec4 vcolor;\n",
+	"\n",
+	"void main()\n",
+	"{\n",
+	"    gl_Position = position;\n",
+	"    vcolor = color;\n",
+	"}"
+};
+
+static const GLchar *fragment_shader[] = {
+	"precision mediump float;\n",
+	"varying vec4 vcolor;\n",
+	"\n",
+	"void main()\n",
+	"{\n",
+	"    gl_FragColor = vcolor;\n",
+	"}"
+};
+
+void pbuffer_draw(struct pbuffer *pbuffer)
+{
+	static const GLfloat vertices[] = {
+		 0.0f,  0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.0f, 1.0f,
+		 0.5f, -0.5f, 0.0f, 1.0f,
+	};
+	static const GLfloat colors[] = {
+		1.0f, 0.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f, 1.0f,
+	};
+	static const GLushort indices[] = {
+		0, 1, 2,
+	};
+	GLuint vs, fs, program;
+	GLint position, color;
+
+	printf("=== compiling shaders\n");
+	vs = glsl_shader_load(GL_VERTEX_SHADER, vertex_shader,
+			      ARRAY_SIZE(vertex_shader));
+	fs = glsl_shader_load(GL_FRAGMENT_SHADER, fragment_shader,
+			      ARRAY_SIZE(fragment_shader));
+	glFlush();
+	printf("=== linking program\n");
+	program = glsl_program_create(vs, fs);
+	glsl_program_link(program);
+	glFlush();
+	printf("=== using program\n");
+	glUseProgram(program);
+	glFlush();
+
+	position = glGetAttribLocation(program, "position");
+	color = glGetAttribLocation(program, "color");
+
+	printf("=== clearing buffer\n");
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+
+	printf("=== setting vertex attribute\n");
+	glVertexAttribPointer(position, 4, GL_FLOAT, GL_FALSE,
+			      4 * sizeof(GLfloat), vertices);
+	glFlush();
+	printf("=== enabling vertex attribute\n");
+	glEnableVertexAttribArray(position);
+	glFlush();
+
+	printf("=== setting vertex attribute\n");
+	glVertexAttribPointer(color, 4, GL_FLOAT, GL_FALSE,
+			      4 * sizeof(GLfloat), colors);
+	glFlush();
+	printf("=== enabling vertex attribute\n");
+	glEnableVertexAttribArray(color);
+	glFlush();
+
+	printf("=== drawing triangles\n");
+	glDrawElements(GL_TRIANGLES, ARRAY_SIZE(indices), GL_UNSIGNED_SHORT,
+		       indices);
+	glFlush();
+	printf("=== done\n");
+}
+
+int main(int argc, char *argv[])
+{
+	struct pbuffer *pbuffer;
+
+	pbuffer = pbuffer_create(32, 32);
+	if (!pbuffer) {
+		fprintf(stderr, "pbuffer_create() failed\n");
+		return 1;
+	}
+
+	pbuffer_draw(pbuffer);
+	pbuffer_save(pbuffer, "test.png");
+	pbuffer_free(pbuffer);
+
+	return 0;
+}
