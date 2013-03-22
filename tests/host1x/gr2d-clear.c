57806c90e132950230977f05f508e4c5bb54e179
Thierry Reding <thierry@gilfi.de>
2013-03-22 16:50:17 +0100

    tests: host1x: gr2d-clear: Add on-screen display support
    If the host1x backend supports on-screen display of framebuffers, prefer
    that over writing out a PNG file.
    
    Signed-off-by: Thierry Reding <thierry@gilfi.de>

--- tests/host1x/gr2d-clear.c
+++ tests/host1x/gr2d-clear.c
@@ -34,33 +34,60 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned int width = 32, height = 32;
+	struct host1x_display *display = NULL;
+	struct host1x_overlay *overlay = NULL;
 	struct host1x_framebuffer *fb;
+	unsigned int width = 256;
+	unsigned int height = 256;
 	struct host1x_gr2d *gr2d;
 	struct host1x *host1x;
 	int err;
 
 	host1x = host1x_open();
 	if (!host1x) {
+		fprintf(stderr, "host1x_open() failed\n");
 		return 1;
+	}
+
+	display = host1x_get_display(host1x);
+	if (display) {
+		err = host1x_overlay_create(&overlay, display);
+		if (err < 0)
+			fprintf(stderr, "overlay support missing\n");
 	}
 
 	gr2d = host1x_get_gr2d(host1x);
 	if (!gr2d) {
+		fprintf(stderr, "host1x_get_gr2d() failed\n");
 		return 1;
 	}
 
 	fb = host1x_framebuffer_create(host1x, width, height, 32, 0);
 	if (!fb) {
+		fprintf(stderr, "host1x_framebuffer_create() failed\n");
 		return 1;
 	}
 
 	err = host1x_gr2d_clear(gr2d, fb, 1.0f, 0.0f, 1.0f, 1.0f);
 	if (err < 0) {
+		fprintf(stderr, "host1x_gr2d_clear() failed: %d\n", err);
 		return 1;
 	}
 
-	host1x_framebuffer_save(fb, "test.png");
+	if (display && overlay) {
+		err = host1x_overlay_set(overlay, fb, 0, 0, width, height);
+		if (err < 0) {
+			fprintf(stderr, "host1x_overlay_set() failed: %d\n",
+				err);
+		} else {
+			sleep(5);
+		}
+
+		host1x_overlay_close(overlay);
+	} else {
+		host1x_framebuffer_save(fb, "test.png");
+	}
+
 	host1x_framebuffer_free(fb);
 	host1x_close(host1x);
 
