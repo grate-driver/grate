75d2e64aec8a8f3f2cd2858afa49fa8752f67fc9
Erik Faye-Lund <kusmabite@gmail.com>
2015-06-26 19:30:53 +0200

    add gles-info for gles1 and gles2
    These just prints extension-strings for now.

---
+++ tests/gles1/gles-info.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2012-2014 Erik Faye-Lund
+ * Copyright (c) 2013 Avionic Design GmbH
+ * Copyright (c) 2013 Thierry Reding
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <GLES/gl.h>
+
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+	struct window *window;
+
+	window = window_create(0, 0, 640, 480);
+	if (!window) {
+		fprintf(stderr, "window_create() failed\n");
+		return 1;
+	}
+
+	printf("GL_EXTENSIONS: %s\n", glGetString(GL_EXTENSIONS));
+
+	window_close(window);
+
+	return 0;
+}
