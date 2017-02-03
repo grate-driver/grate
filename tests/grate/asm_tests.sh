8c348a255f0f7fd3d398592b8481864f8ba9dc0b
Dmitry Osipenko <digetx@gmail.com>
2017-02-04 00:44:26 +0300

    tests/grate: asm: add vertex uniform test

--- tests/grate/asm_tests.sh
+++ tests/grate/asm_tests.sh
@@ -9,4 +9,5 @@ DIR=$(dirname $0)
 &&	$DIR/../../tools/assembler --vs $DIR/asm/vs_constant_relative_addressing.txt  --fs $DIR/asm/fs_vs_tests.txt --lnk $DIR/asm/linker_vs_tests.txt --expected 0x664D331A --testonly \
 &&	$DIR/../../tools/assembler --vs $DIR/asm/vs_attribute_relative_addressing.txt --fs $DIR/asm/fs_vs_tests.txt --lnk $DIR/asm/linker_vs_tests.txt --expected 0xFF01FF00 --testonly \
 &&	$DIR/../../tools/assembler --vs $DIR/asm/vs_export_relative_addressing.txt    --fs $DIR/asm/fs_vs_tests.txt --lnk $DIR/asm/linker_vs_tests.txt --expected 0x6601FF1A --testonly \
+&&	$DIR/../../tools/assembler --vs $DIR/asm/vs_uniform.txt  --fs $DIR/asm/fs_vs_tests.txt --lnk $DIR/asm/linker_vs_tests.txt --expected 0x664D331A --vs_uniform '["uniform_100"]=(0.1,0.2,0.0,0.0)' --vs_uniform '["uniform_135"]=(0.0,0.0,0.3,0.4)' --testonly \
 &&	echo "All tests passed"
