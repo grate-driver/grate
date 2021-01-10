f58076b17436976360af62a5a2f39e67188b7ea1
Dmitry Osipenko <digetx@gmail.com>
2021-01-10 21:39:46 +0300

    tools/reset3d: reset second 3d block
    Tegra30+ has two identical 3d engines which are connected in a SLI.
    By default the even horizontal lines of 16x16 blocks are drawn by the
    first engine and the odd by the second. The second engine has a separate
    reset control. This control is a NO-OP on Tegra20.

--- tools/reset3d.c
+++ tools/reset3d.c
@@ -7,10 +7,17 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
+#define CLK_RST_CONTROLLER_RST_DEV_L		0x004
+#define CLK_RST_CONTROLLER_RST_DEV_V		0x058
+
 #define CLK_RST_CONTROLLER_RST_DEV_L_SET_0	0x300
 #define CLK_RST_CONTROLLER_RST_DEV_L_CLR_0	0x304
 
+#define CLK_RST_CONTROLLER_RST_DEV_V_SET_0	0x430
+#define CLK_RST_CONTROLLER_RST_DEV_V_CLR_0	0x434
+
 #define CAR_3D	(1 << 24)
+#define CAR_3D2	(1 << 2)
 
 static int map_mem(void **mem_virt, off_t phys_address, int size)
 {
@@ -37,6 +44,11 @@ static int map_mem(void **mem_virt, off_t phys_address, int size)
 	return 0;
 }
 
+static uint32_t reg_read(void *mem_virt, uint32_t offset)
+{
+	return *(volatile uint32_t*)(mem_virt + offset);
+}
+
 static void reg_write(void *mem_virt, uint32_t offset, uint32_t value)
 {
 	*(volatile uint32_t*)(mem_virt + offset) = value;
@@ -54,13 +66,25 @@ int main(int argc, char *argv[])
 		return err;
 	}
 
+	printf("GR3D1 reset = %d\n",
+	       !!(reg_read(CAR_io_mem_virt, CLK_RST_CONTROLLER_RST_DEV_L) & CAR_3D));
+
+	printf("GR3D2 reset = %d\n",
+	       !!(reg_read(CAR_io_mem_virt, CLK_RST_CONTROLLER_RST_DEV_V) & CAR_3D2));
+
 	reg_write(CAR_io_mem_virt,
 		  CLK_RST_CONTROLLER_RST_DEV_L_SET_0, CAR_3D);
+
+	reg_write(CAR_io_mem_virt,
+		  CLK_RST_CONTROLLER_RST_DEV_V_SET_0, CAR_3D2);
 
 	usleep(1000);
 
 	reg_write(CAR_io_mem_virt,
 		  CLK_RST_CONTROLLER_RST_DEV_L_CLR_0, CAR_3D);
+
+	reg_write(CAR_io_mem_virt,
+		  CLK_RST_CONTROLLER_RST_DEV_V_CLR_0, CAR_3D2);
 
 	printf("done\n");
 
